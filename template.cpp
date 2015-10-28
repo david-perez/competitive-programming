49ebf1bf4fd9f3dedc053889450757b3fbae056c
= <=>
2015-10-28 23:02:43 +0100

    template

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> ll_ll;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+
+int main() {
+
+    return 0;
+}
