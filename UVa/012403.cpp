1e86702e6e0a5ea11c8f558ad1df0f20f07f6338
david-perez <hallaplay835@gmail.com>
2015-11-04 23:46:26 +0100

    12403 - Save Setu https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3834

---
+++ UVa/012403.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> ll_ll;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+
+int main() {
+    int n;
+    cin >> n;
+    string word; int buff; int total = 0;
+    while (n--) {
+        cin >> word;
+        if (word == "report") cout << total << endl;
+        if (word == "donate") {
+            cin >> buff;
+            total += buff;
+        }
+    }
+    
+    return 0;
+}
