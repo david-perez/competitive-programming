741332ae1ded8514d1df4fd3df436663f3bb9518
david-perez <hallaplay835@gmail.com>
2015-11-07 22:33:20 +0100

    10055 - Hashmat the Brave Warrior https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=996

---
+++ UVa/010055.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> ll_ll;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+
+int main() {
+    unsigned long x, y;
+    while(scanf("%ld %ld", &x, &y) == 2) cout << max(x, y) - min(x, y) << endl;
+
+    return 0;
+}
