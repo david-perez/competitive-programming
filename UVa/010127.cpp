9aa5bc489623bbe6cf97d6d64ed7564da8991b7c
david-perez <hallaplay835@gmail.com>
2015-11-27 19:55:37 +0100

    10127 - Ones https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1068

---
+++ UVa/010127.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> ll_ll;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+
+int main() {
+    int n;
+    while (cin >> n) {
+        int t = 1;
+        unsigned long long x = 1;
+        while (t % n != 0) {
+            t = t * 10 + 1;
+            t = t % n;
+            x++;
+        }
+        cout << x << endl;
+    }
+
+    return 0;
+}
