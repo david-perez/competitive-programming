96615b6dbf2897c2f0863363d71dc19969fe81d2
david-perez <hallaplay835@gmail.com>
2015-12-18 00:36:26 +0100

    pair of double and vector of pair of double macros

--- template.cpp
+++ template.cpp
@@ -24,9 +24,11 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> ii;
 typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
 typedef vector<int> vi;
 typedef map<int, int> mii;
 typedef vector<ii> vii;
+typedef vector<dd> vdd;
 typedef vector<ll> vll;
 typedef vector<vi> vvi;
 
