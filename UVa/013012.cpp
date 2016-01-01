40e66e11e501598badcdf00af13740a4ef6cea85
david-perez <hallaplay835@gmail.com>
2016-01-01 17:28:32 +0100

    13012 - Identifying tea https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=4900 Problem L - Las 12 UVas 2015-16

---
+++ UVa/013012.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+
+int main() {
+    int t, u;
+    while(scanf("%d", &t) == 1) {
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            cin >> u;
+            if (u == t) count++;
+        }
+        
+        cout << count << endl;
+    }
+    
+    return 0;
+}
