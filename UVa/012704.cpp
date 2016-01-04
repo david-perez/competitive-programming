b7a23a5ba8d04267600054fdab41905d489e5cfd
david-perez <hallaplay835@gmail.com>
2016-01-04 14:22:29 +0100

    12704 - Little Masters https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=4442 Problem I - Las 12 UVas 2015-16

---
+++ UVa/012704.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+
+int main() {
+    int t;
+    double x, y, r;
+    
+    cin >> t;
+    while (t--) {
+        cin >> x >> y >> r;
+        double d = hypot(x, y);
+        printf("%.2f %.2f\n", r - d, 2*r-(r-d));
+    }
+
+    return 0;
+}
