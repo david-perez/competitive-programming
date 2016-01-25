42a0dbe311139a33b90766cec09c3871fdfe8247
david-perez <hallaplay835@gmail.com>
2016-01-25 07:42:57 +0100

    67 - Maximum path sum II https://projecteuler.net/problem=67

---
+++ project-euler/0067.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+
+const int size = 100;
+
+int main() {
+    freopen("0067.txt", "r", stdin);
+    int triangle[size][size];
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j <= i; j++)
+            cin >> triangle[i][j];
+    
+    for (int i = size-2; i >= 0; i--)
+        for (int j = 0; j <= i; j++)
+            triangle[i][j] += max(triangle[i+1][j], triangle[i+1][j+1]);   
+    
+    cout << triangle[0][0] << endl;
+    
+    return 0;
+}
