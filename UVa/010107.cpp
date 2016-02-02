09a99dc815a9f5d02ed3d91aa7bb54695d0d8113
david-perez <hallaplay835@gmail.com>
2016-02-02 23:31:38 +0100

    10107 - What is the Median? https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1048

---
+++ UVa/010107.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+
+int main() {
+    long n;
+    vector<long> v;
+    while (cin >> n) {
+        v.push_back(n);
+        sort(v.begin(), v.end());
+        cout << (v.size() % 2 == 0 ? (v[v.size()/2]+v[v.size()/2-1])/2 : v[v.size()/2]) << endl;
+    }
+    
+    return 0;
+}
