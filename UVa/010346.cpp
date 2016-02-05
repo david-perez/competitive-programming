c7c345a838d89a7eefc0f5bd5932fefe0573e949
david-perez <hallaplay835@gmail.com>
2016-02-05 23:44:14 +0100

    10346 - Peter's Smokes https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1287

---
+++ UVa/010346.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+
+int f(int n, int k) {
+    if (n < k) return n;
+    return (n - n%k) + f(n/k + n%k, k);
+}
+
+int main() {
+    int n, k;
+    while (scanf("%d %d", &n, &k) == 2)
+        cout << f(n, k) << endl;
+    
+    return 0;
+}
