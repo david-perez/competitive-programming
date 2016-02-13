b67717cca020149535745f1069ade1f11c4cc6e6
david-perez <hallaplay835@gmail.com>
2016-02-13 23:07:57 +0100

    424 - Integer Inquiry https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=365

---
+++ UVa/000424.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger num = BigInteger.ZERO;
+        while (in.hasNext()) {
+            BigInteger buff = in.nextBigInteger();
+            if (buff.equals(BigInteger.ZERO)) break;
+            num = num.add(buff);
+        }
+        System.out.println(num);
+    }
+}
