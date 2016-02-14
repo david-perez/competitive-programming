b3d95a11b4f854094dee843c85e0f60bb9b2b264
david-perez <hallaplay835@gmail.com>
2016-02-14 13:39:35 +0100

    156 - Ascensor https://www.aceptaelreto.com/problem/statement.php?id=156

---
+++ acepta-el-reto/0156.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<long, long> l_l;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<l_l> vl_l;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, long> mil;
+typedef map<long, long> mll;
+
+int main() {
+    int a, b;
+    while (scanf("%d", &a) && a >= 0) {
+        int l = 0;
+        while (scanf("%d", &b) && b != -1) {
+            l += abs(b-a);
+            a = b;
+        }
+        cout << l << endl;
+    }
+    
+    return 0;
+}
