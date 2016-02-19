89d04d62e8cadc8c053cfd0d2a922703ec8f4617
david-perez <hallaplay835@gmail.com>
2016-02-19 23:10:32 +0100

    12372 - Packing for Holiday https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3794

---
+++ UVa/012372.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<long, long> l_l;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<l_l> vl_l;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, long> mil;
+typedef map<long, long> mll;
+
+int main() {
+    int t, l, w, h;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        cin >> l >> w >> h;
+        printf("Case %d: %s\n", i, l <= 20 && w <= 20 && h <= 20 ? "good" : "bad");
+    }
+
+    return 0;
+}
