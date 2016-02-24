9153069f2247ed656279f0b62833281b44375e2b
david-perez <hallaplay835@gmail.com>
2016-02-24 23:34:15 +0100

    11185 - Ternary https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2126

---
+++ UVa/011185.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<long, long> l_l;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<l_l> vl_l;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, long> mil;
+typedef map<long, long> mll;
+
+int main() {
+    long long n;
+    while (cin >> n && n >= 0) {
+        if (n == 0) {
+            cout << 0 << endl;
+            continue;
+        }
+
+        stack<int> s;
+        while (n != 0) {
+            s.push(n % 3);
+            n /= 3;
+        }
+
+        while (!s.empty()) {
+            cout << s.top();
+            s.pop();
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
