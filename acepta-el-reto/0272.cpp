4c60c54758500d4d45c642fec0afca187ee1429a
david-perez <hallaplay835@gmail.com>
2016-02-26 23:35:43 +0100

    272 - Tres dedos en cada mano https://www.aceptaelreto.com/problem/statement.php?id=272 Problema A - Ada Byron 2016 fase de prueba

---
+++ acepta-el-reto/0272.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<long, long> l_l;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<l_l> vl_l;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, long> mil;
+typedef map<long, long> mll;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        if (n == 0) {
+            cout << 0 << endl;
+            continue;
+        }
+
+        stack<int> s;
+        while (n != 0) {
+            s.push(n % 6);
+            n /= 6;
+        }
+
+        while (!s.empty())
+            cout << s.top(), s.pop();
+        cout << endl;
+    }
+
+    return 0;
+}
