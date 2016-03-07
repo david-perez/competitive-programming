46bf0a3a0ff4fcfd3cd0277d53662427db527db8
david-perez <hallaplay835@gmail.com>
2016-03-07 17:43:46 +0100

    long type in template removed/modified to long long (long type in 64-bit archs is equivalent to int)

--- template.cpp
+++ template.cpp
@@ -25,21 +25,18 @@ typedef long long ll;
 typedef pair<int, int> ii;
 typedef pair<int, double> id;
 typedef pair<double, int> di;
-typedef pair<long, long> l_l;
 typedef pair<ll, ll> ll_ll;
 typedef pair<double, double> dd;
 typedef vector<int> vi;
-typedef vector<long> vl;
 typedef vector<double> vd;
 typedef vector<ll> vll;
 typedef vector<ii> vii;
 typedef vector<dd> vdd;
 typedef vector<id> vid;
-typedef vector<l_l> vl_l;
 typedef vector<vi> vvi;
 typedef map<int, int> mii;
-typedef map<int, long> mil;
-typedef map<long, long> mll;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
 
 int main() {
     //freopen("input.txt", "r", stdin);
