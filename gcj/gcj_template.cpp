cc8cf97cd53b0d327a1169b51a5687dbf64b914c
david-perez <hallaplay835@gmail.com>
2016-03-17 23:01:52 +0100

    GCJ template

---
+++ gcj/gcj_template.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+//#define ONLINE_JUDGE
+
+int main() {
+    #ifdef ONLINE_JUDGE
+        //freopen("X-small-practice.in", "r", stdin);
+        //freopen("X-small-practice.out", "w", stdout);
+        freopen("X-large-practice.in", "r", stdin);
+        freopen("X-large-practice.out", "w", stdout);
+    #endif
+
+    return 0;
+}
