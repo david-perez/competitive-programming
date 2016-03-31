66ca9ae3d647d6c69530cf899d240cb92d420c9c
david-perez <hallaplay835@gmail.com>
2016-03-31 23:07:30 +0200

    11849 - CD https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2949

---
+++ UVa/011849.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+int main() {
+    int N, M;
+    while (cin >> N >> M && !(N == 0 && M == 0)) {
+        set<int> jack;
+        while (N--) {
+            int cd; cin >> cd;
+            jack.insert(cd);
+        }
+        int sold = 0;
+        while (M--) {
+            int cd; cin >> cd;
+            if (jack.count(cd)) sold++;
+        }
+        cout << sold << endl;
+    }
+
+    return 0;
+}
