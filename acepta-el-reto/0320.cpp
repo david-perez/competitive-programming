37724df8026dfb5236b5b21112d07af53fe7179e
david-perez <hallaplay835@gmail.com>
2016-04-09 23:14:53 +0200

    13070 - Palm trees in the snow https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=4968 320 - Palmeras en la nieve https://www.aceptaelreto.com/problem/statement.php?id=320&cat=91 Problema B - Ada Byron 2016

---
+++ acepta-el-reto/0320.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+int main() {
+    int T;
+    cin >> T;
+    int p[100000];
+    while (T--) {
+        int W, n; cin >> W >> n;
+        int i = 0, j = 0, l = 0, enpie = 0;
+        while (n--) {
+            cin >> p[j];
+            if (p[j] >= W && ++enpie > 5)
+                while (enpie > 5) {
+                    if (p[i] >= W) enpie--;
+                    i++;
+                }
+            l = max(l, j-i+1);
+            j++;
+        }
+        cout << l << endl;
+    }
+
+    return 0;
+}
