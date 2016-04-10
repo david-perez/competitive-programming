e67727b291777d3d831e8dbbff12346ebe12abce
david-perez <hallaplay835@gmail.com>
2016-04-10 18:35:45 +0200

    Main class of UVa 424 has to be non-public in order to run a main method of a .java file whose filename does not match the class name of the class it defines

--- UVa/000424.java
+++ UVa/000424.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 import java.util.Scanner;
 
-public class Main {
+class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
