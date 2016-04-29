80bbfd469a67202221b4a2fb440e2d55479b2afe
david-perez <hallaplay835@gmail.com>
2016-04-30 00:07:56 +0200

    114 - Ultimo dígito del factorial https://www.aceptaelreto.com/problem/statement.php?id=114

---
+++ acepta-el-reto/0114.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+int main() {
+    int T; cin >> T;
+    while (T--) {
+        int n; cin >> n;
+        if (n == 0) cout << 1 << endl;
+        else if (n == 3) cout << 6 << endl;
+        else if (n < 5) cout << n << endl;
+        else cout << 0 << endl;
+    }
+
+    return 0;
+}
