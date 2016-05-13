fce5060e076e0620e4a25440fc75aef7ce4ca897
david-perez <hallaplay835@gmail.com>
2016-05-13 23:24:08 +0200

    530 - Binomial Showdown https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=471

---
+++ UVa/000530.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+int main() {
+    int n, k;
+    while (cin >> n >> k && !(n == 0 && k == 0)) {
+        k = min(k, n-k);
+        ll nck = 1;
+        for (int i = 1; i <= k; i++)
+            nck = nck * (n - k + i) / i;
+        cout << nck << endl;
+    }
+
+    return 0;
+}
