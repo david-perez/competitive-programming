520b1b30135a2a1612f9ecb0d75f56c132706e77
david-perez <hallaplay835@gmail.com>
2016-05-22 22:18:59 +0200

    10282 - Babelfish https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1223

---
+++ UVa/010282.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+int main() {
+    map<string, string> dict;
+    string s;
+    while (getline(cin, s) && s.length() > 0) {
+        stringstream ss(s);
+        string s1, s2; ss >> s1 >> s2;
+        dict[s2] = s1;
+    }
+
+    while (getline(cin, s)) {
+        auto it = dict.find(s);
+        if (it == dict.end()) cout << "eh" << endl;
+        else cout << it->second << endl;
+    }
+
+    return 0;
+}
