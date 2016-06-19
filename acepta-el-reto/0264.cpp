c59e5c0e8ba5392fd2889bdb82b58dedf1a64754
david-perez <hallaplay835@gmail.com>
2016-06-19 21:36:46 +0200

    264 - Pirámide de fichas de dominó https://www.aceptaelreto.com/problem/statement.php?id=264

---
+++ acepta-el-reto/0264.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+int main() {
+    ll n;
+    while (cin >> n) {
+        ll a, r;
+        for (r = sqrt(2*n); r >= 1; r--) {
+            a = (2*n - r*(r-1)) / (2*r);
+            if (2*n == r * (2*a + r - 1)) break;
+        }
+
+        cout << r << endl;
+    }
+
+    return 0;
+}
