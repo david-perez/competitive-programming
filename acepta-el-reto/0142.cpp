40e2ada4b963dd1ddc87f2b5b4c34879dd6f6fae
david-perez <hallaplay835@gmail.com>
2016-07-08 17:23:47 +0200

    142 - ¿Quién empieza? https://www.aceptaelreto.com/problem/statement.php?id=142

---
+++ acepta-el-reto/0142.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+int main() {
+    int n, m;
+    while (cin >> n >> m && !(n == 0 && m == 0)) {
+        queue<int> q;
+        for (int i = 1; i <= n; i++)
+            q.push(i);
+
+        while (q.size() != 1) {
+            for (int i = 0; i < m; i++) {
+                q.push(q.front());
+                q.pop();
+            }
+
+            q.pop();
+        }
+
+        cout << q.front() << endl;
+    }
+
+    return 0;
+}
