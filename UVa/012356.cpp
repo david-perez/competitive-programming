81312be38be584075056d9fd9d78b19e6ecaffb9
david-perez <hallaplay835@gmail.com>
2016-07-20 21:07:56 +0200

    12356 - Army Buddies https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=3778

---
+++ UVa/012356.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+#define INF 1000000000 // 10^9
+
+int lefts[100001], rights[100001];
+
+int main() {
+    int S, B;
+    while (cin >> S >> B && !(S == 0 && B == 0)) {
+        for (int i = 1; i <= S; i++) {
+            lefts[i] = i-1;
+            rights[i] = i+1;
+        }
+        rights[S] = lefts[1] = -1;
+
+        while (B--) {
+            int L, R;
+            cin >> L >> R;
+
+            lefts[rights[R]] = lefts[L];
+            if (lefts[L] == -1) cout << '*';
+            else cout << lefts[L];
+            cout << ' ';
+            rights[lefts[L]] = rights[R];
+            if (rights[R] == -1) cout << '*';
+            else cout << rights[R];
+
+            cout << endl;
+        }
+
+        cout << '-' << endl;
+    }
+
+    return 0;
+}
