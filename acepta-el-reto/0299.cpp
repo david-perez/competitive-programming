9403a20a956e7e9654c17589da6730c1e64c6df6
david-perez <hallaplay835@gmail.com>
2016-07-27 23:28:17 +0200

    299 - Pavimentar Barro City https://www.aceptaelreto.com/problem/statement.php?id=299

---
+++ acepta-el-reto/0299.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+#define INF 1000000000 // 10^9
+
+struct ufds {
+    vi p, r;
+    int numSets;
+
+    ufds(int N) {
+        p.assign(N, 0); r.assign(N, 0); numSets = N;
+        for (int i = 0; i < N; i++) p[i] = i;
+    }
+
+    int findSet(int i) { // finds and path compresses if possible
+        return (p[i] == i) ? i : (p[i] = findSet(p[i]));
+    }
+
+    bool isSameSet(int i, int j) {
+        return findSet(i) == findSet(j);
+    }
+
+    void unionSet(int i, int j) {
+        int x = findSet(i), y = findSet(j);
+        if (x != y) {
+            if (r[x] > r[y]) p[y] = x;
+            else {
+                p[x] = y;
+                if (r[x] == r[y]) r[y]++;
+            }
+            numSets--;
+        }
+    }
+
+    void print() {
+        cout << "p[] = ";
+        for (int i = 0; i < p.size(); i++)
+            cout << i << " -> " << p[i] << "; ";
+        cout << endl;
+    }
+};
+
+int V, E;
+
+int kruskal(vector<pair<int, ii>> &edgeList) {
+    sort(edgeList.begin(), edgeList.end());
+    int mst_cost = 0;
+    ufds uf(V);
+
+    for (int i = 0; i < E && uf.numSets != 1; i++) {
+        pair<int, ii> front = edgeList[i];
+        if (!uf.isSameSet(front.second.first, front.second.second)) {
+            mst_cost += front.first;
+            uf.unionSet(front.second.first, front.second.second);
+        }
+    }
+
+    if (uf.numSets != 1) return INT_MAX; // this graph is not connected
+    return mst_cost;
+}
+
+int main() {
+    while (cin >> V >> E) {
+        vector<pair<int, ii>> edgeList;
+
+        for (int i = 0; i < E; i++) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            u--, v--;
+            edgeList.push_back(make_pair(w, ii(u, v)));
+        }
+
+        int mst_cost = kruskal(edgeList);
+
+        if (mst_cost == INT_MAX) cout << "Imposible" << endl;
+        else cout << mst_cost << endl;
+    }
+
+    return 0;
+}
