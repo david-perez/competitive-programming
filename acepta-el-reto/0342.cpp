90c21aad449f87b9846a5e068ecda0954e10cec3
david-perez <hallaplay835@gmail.com>
2016-07-30 00:20:42 +0200

    342 - ¡No lo puedes saber! https://www.aceptaelreto.com/problem/statement.php?id=342

---
+++ acepta-el-reto/0342.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+#define INF 1000000000 // 10^9
+
+int main() {
+    int ini, fin, n;
+    while (cin >> ini >> fin >> n && !(ini == 0 && fin == 0 && n == 0)) {
+        int k;
+        cin >> k;
+        while (k--) {
+            int p;
+            cin >> p;
+            if (p < ini || p > fin) continue;
+            if (p <= n) ini = p;
+            else if (n < p) fin = p - 1;
+        }
+
+        cout << (ini == fin ? "LO SABE" : "NO LO SABE") << endl;
+    }
+
+    return 0;
+}
