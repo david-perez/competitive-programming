719b464db85afc940c3af0c1ebf8078aa356c08b
david-perez <hallaplay835@gmail.com>
2016-08-16 10:30:02 +0000

    13109 - Elephants https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=5020

---
+++ UVa/013109.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int M, W;
+        cin >> M >> W;
+        vi v;
+
+        while (M--) {
+            int x; cin >> x;
+            v.push_back(x);
+        }
+
+        sort(v.begin(), v.end());
+        int sum = 0, cnt = 0;
+        for (int i = 0; i < v.size(); i++) {
+            if (sum + v[i] <= W) sum += v[i], cnt++;
+            else break;
+        }
+
+        cout << cnt << endl;
+    }
+
+    return 0;
+}
