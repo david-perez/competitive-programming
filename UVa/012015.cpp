94a56bbc4e45d31c590f0834287d645e9f24f503
david-perez <hallaplay835@gmail.com>
2016-08-25 13:00:02 +0000

    12015 - Google is Feeling Lucky https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=3166

---
+++ UVa/012015.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+#define INF 1000000000 // 10^9
+#define PI acos(-1)
+#define EPS 1e-9
+
+int main() {
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; t++) {
+        vector<string> v;
+        int M = 0;
+        for (int i = 0; i < 10; i++) {
+            string s;
+            int x;
+            cin >> s >> x;
+            if (x > M) {
+                v.clear();
+                v.push_back(s);
+                M = x;
+            } else if (x == M)
+                v.push_back(s);
+        }
+
+        printf("Case #%d:\n", t);
+        for (string &s : v)
+            cout << s << endl;
+    }
+
+    return 0;
+}
