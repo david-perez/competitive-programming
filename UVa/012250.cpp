145ab64467756fa2f2533d2cfb261cb59ca997b6
david-perez <hallaplay835@gmail.com>
2016-08-26 13:00:02 +0000

    12250 - Language Detection https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=3402

---
+++ UVa/012250.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+#define INF 1000000000 // 10^9
+#define PI acos(-1)
+#define EPS 1e-9
+
+int main() {
+    int t = 1;
+    string s;
+    while (cin >> s && s != "#") {
+        printf("Case %d: ", t++);
+        if (s == "HELLO") cout << "ENGLISH" << endl;
+        else if (s == "HOLA") cout << "SPANISH" << endl;
+        else if (s == "HALLO") cout << "GERMAN" << endl;
+        else if (s == "BONJOUR") cout << "FRENCH" << endl;
+        else if (s == "CIAO") cout << "ITALIAN" << endl;
+        else if (s == "ZDRAVSTVUJTE") cout << "RUSSIAN" << endl;
+        else cout << "UNKNOWN" << endl;
+    }
+
+    return 0;
+}
