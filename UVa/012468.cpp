a1f31f14a78df04a9cb3fc5249ab55ca8954ec8f
david-perez <hallaplay835@gmail.com>
2016-08-27 13:00:02 +0000

    12468 - Zapping https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=3912

---
+++ UVa/012468.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+#define INF 1000000000 // 10^9
+#define PI acos(-1)
+#define EPS 1e-9
+
+int main() {
+    int a, b;
+    while (cin >> a >> b && !(a == -1 && b == -1)) {
+        int c = a, d = b;
+        a = min(c, d);
+        b = max(c, d);
+        cout << min(b - a, a + 100 - b) << endl;
+    }
+
+    return 0;
+}
