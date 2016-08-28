abf7c89f23b2878f816bb068108b9a87e2cd36b3
david-perez <hallaplay835@gmail.com>
2016-08-28 13:00:02 +0000

    12503 - Robot Instructions https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=3947

---
+++ UVa/012503.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+#define INF 1000000000 // 10^9
+#define PI acos(-1)
+#define EPS 1e-9
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        vi v;
+        int p = 0;
+        while (N--) {
+            string s;
+            cin >> s;
+            if (s == "LEFT") p--, v.push_back(-1);
+            else if (s == "RIGHT") p++, v.push_back(1);
+            else {
+                cin >> s;
+                int x;
+                cin >> x;
+                p += v[x-1];
+                v.push_back(v[x-1]);
+            }
+        }
+
+        cout << p << endl;
+    }
+
+    return 0;
+}
