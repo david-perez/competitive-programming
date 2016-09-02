10171fe258d1df21dee2a20ef5d38db32f17c1fe
david-perez <hallaplay835@gmail.com>
2016-09-02 13:00:02 +0000

    11936 - The Lazy Lumberjacks https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=3087

---
+++ UVa/011936.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+#define INF 1000000000 // 10^9
+#define PI acos(-1)
+#define EPS 1e-9
+
+int main() {
+    int N;
+    cin >> N;
+    while (N--) {
+        int x[3];
+        cin >> x[0] >> x[1] >> x[2];
+        sort(x, x+ 3);
+
+        if (x[0] > 0 && x[2] < x[0] + x[1])
+            cout << "OK" << endl;
+        else
+            cout << "Wrong!!" << endl;
+    }
+
+    return 0;
+}
