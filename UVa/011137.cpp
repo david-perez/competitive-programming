848a346e170135c525424b29f2094008c5bd54b5
david-perez <hallaplay835@gmail.com>
2016-10-18 13:00:02 +0000

    11137 - Ingenuous Cubrency https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=2078

---
+++ UVa/011137.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+#include <numeric>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+#define INF 1000000000 // 10^9
+#define PI acos(-1)
+#define EPS 1e-9
+
+int main() {
+    vi cubes;
+    for (int i = 1; i*i*i <= 9261; i++)
+        cubes.push_back(i*i*i);
+
+    ll dp[10000];
+    memset(dp, 0, sizeof dp);
+    dp[0] = 1;
+
+    for (int &c : cubes)
+        for (int v = c; v < 10000; v++)
+            dp[v] += dp[v - c];
+
+    int n;
+    while (cin >> n)
+        cout << dp[n] << endl;
+
+    return 0;
+}
