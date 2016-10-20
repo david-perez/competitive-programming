c9cfb118c75df31c21443564c925a50772990aaa
david-perez <hallaplay835@gmail.com>
2016-10-20 18:41:35 +0200

    10209 - Is This Integration ? https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1150

---
+++ UVa/010209.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+#include <numeric>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+#define INF 1000000000 // 10^9
+#define PI acos(-1)
+#define EPS 1e-9
+
+int main() {
+    double a;
+    while (cin >> a) {
+        double x = 1.0/3*(PI - 3*sqrt(3) + 3)*a*a;
+        double y = 1.0/12*(PI + 6*sqrt(3) - 12)*a*a;
+        double z = -1.0/12*(2*PI + 3*sqrt(3) - 12)*a*a;
+        printf("%.3f %.3f %.3f\n", x, 4*y, 4*z);
+    }
+
+    return 0;
+}
