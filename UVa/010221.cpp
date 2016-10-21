85c2aee5350bfbd69e6d64c21c2f026060ee939f
david-perez <hallaplay835@gmail.com>
2016-10-21 13:00:02 +0000

    10221 - Satellites https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1162

---
+++ UVa/010221.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <algorithm> // max...
+#include <utility> // pair
+#include <complex>
+#include <climits> // int, ll...
+#include <limits> // double...
+#include <cmath> // abs, atan...
+#include <cstring> // memset
+#include <string>
+#include <functional> // greater, less...
+#include <bitset>
+#include <numeric>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, double> id;
+typedef pair<double, int> di;
+typedef pair<ll, ll> ll_ll;
+typedef pair<double, double> dd;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<id> vid;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<int, ll> mil;
+typedef map<ll, ll> mll;
+
+#define INF 1000000000 // 10^9
+#define PI acos(-1)
+#define EPS 1e-9
+
+int main() {
+    double s, a, r = 6440;
+    string t;
+    while (cin >> s >> a >> t) {
+        if (t == "min") a /= 60;
+        a *= PI / 180.0;
+        a = min(a, 2*PI - a);
+
+        double l = (s+r) * a;
+        double d = 2*(s+r)*sin(a/2);
+        printf("%.6f %.6f\n", l, d);
+    }
+
+    return 0;
+}
