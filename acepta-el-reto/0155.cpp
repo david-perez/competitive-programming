fca071318f29e1e7905c879bcb3d4ad7c22434c9
david-perez <hallaplay835@gmail.com>
2016-11-23 23:53:43 +0100

    155 - Perímetro de un rectángulo https://www.aceptaelreto.com/problem/statement.php?id=155

---
+++ acepta-el-reto/0155.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int a, b;
+  while (cin >> a >> b && !(a < 0 || b < 0))
+    cout << 2*(a+b) << endl;
+
+	return 0;
+}
