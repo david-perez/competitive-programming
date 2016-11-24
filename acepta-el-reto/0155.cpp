d053cb028e92d268262d92865bb0f1bd188c93b2
david-perez <hallaplay835@gmail.com>
2016-11-24 18:42:24 +0100

    Fix indentation in acer 155

--- acepta-el-reto/0155.cpp
+++ acepta-el-reto/0155.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main() {
-	int a, b;
-  while (cin >> a >> b && !(a < 0 || b < 0))
-    cout << 2*(a+b) << endl;
+    int a, b;
+    while (cin >> a >> b && !(a < 0 || b < 0))
+        cout << 2*(a+b) << endl;
 
 	return 0;
 }
