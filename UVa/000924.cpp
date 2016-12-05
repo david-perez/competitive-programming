79e6c1561d2dec88ef9f7a3e3f45103b2e55658a
david-perez <hallaplay835@gmail.com>
2016-12-05 22:23:07 +0100

    Refactor UVa 924

--- UVa/000924.cpp
+++ UVa/000924.cpp
@@ -1,53 +1,19 @@
 #include <iostream>
-#include <iomanip>
-#include <sstream>
-#include <cstdlib>
-#include <cstdio>
 #include <vector>
 #include <queue>
-#include <map>
-#include <set>
-#include <stack>
-#include <algorithm> // max...
-#include <utility> // pair
-#include <complex>
-#include <climits> // int, ll...
-#include <limits> // double...
-#include <cmath> // abs, atan...
-#include <cstring> // memset
-#include <string>
-#include <functional> // greater, less...
-#include <bitset>
+#include <climits>
 
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> ii;
-typedef pair<int, double> id;
-typedef pair<double, int> di;
-typedef pair<long, long> l_l;
-typedef pair<ll, ll> ll_ll;
-typedef pair<double, double> dd;
 typedef vector<int> vi;
-typedef vector<long> vl;
-typedef vector<double> vd;
-typedef vector<ll> vll;
-typedef vector<ii> vii;
-typedef vector<dd> vdd;
-typedef vector<id> vid;
-typedef vector<l_l> vl_l;
 typedef vector<vi> vvi;
-typedef map<int, int> mii;
-typedef map<int, long> mil;
-typedef map<long, long> mll;
 
-void bfs(vvi &adjList, int s) {
-    vi d(adjList.size(), INT_MAX);
-    d[s] = 0;
+void bfs(vector<vi> &adjList, int u, vi &d) {
+    d[u] = 0;
     queue<int> q;
-    q.push(s);
+    q.push(u);
     while (!q.empty()) {
-        int u = q.front(); q.pop();
+        u = q.front(); q.pop();
         for (int j = 0; j < adjList[u].size(); j++) {
             int v = adjList[u][j];
             if (d[v] == INT_MAX) {
@@ -56,44 +22,44 @@ void bfs(vvi &adjList, int s) {
             }
         }
     }
-
-    sort(d.begin(), d.end());
-    int M = 0, D = 0, i = 1;
-    while (i < d.size() && d[i] != INT_MAX) {
-        int t = d[i], boom = 0;
-        while (d[i] == t) {
-            boom++;
-            i++;
-        }
-        if (boom > M) {
-            M = boom;
-            D = t;
-        }
-    }
-
-    if (M == 0) cout << 0 << endl;
-    else printf("%d %d\n", M, D);
 }
 
 int main() {
-    int V;
-    cin >> V;
-    vvi adjList(V, vi());
-    for (int u = 0; u < V; u++) {
-        int N, v;
+    int E;
+    cin >> E;
+    vvi adjList(E, vi());
+    for (int u = 0; u < E; u++) {
+        int N;
         cin >> N;
         while (N--) {
+            int v;
             cin >> v;
             adjList[u].push_back(v);
         }
     }
 
-    int T, s;
+    int T;
     cin >> T;
     while (T--) {
+        vi d(E, INT_MAX);
+        int s;
         cin >> s;
-        bfs(adjList, s);
-    }
+        bfs(adjList, s, d);
 
-    return 0;
+        vi cnt(E, 0);
+        for (int &day : d)
+            if (day != INT_MAX)
+                cnt[day]++;
+
+        int M = 0;
+        int D = -1;
+        for (int day = 1; day < E; day++)
+            if (cnt[day] > M) {
+                M = cnt[day];
+                D = day;
+            }
+
+        if (D == -1) cout << 0 << endl;
+        else printf("%d %d\n", M, D);
+    }
 }
