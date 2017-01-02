7c3f7afa983630418337d87cb1a93e359e21513c
david-perez <hallaplay835@gmail.com>
2017-01-02 22:48:30 +0100

    369 - Contando en la arena https://www.aceptaelreto.com/problem/statement.php?id=369 Problem B - Las 12 UVas 2016-17

---
+++ acepta-el-reto/0369.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    while (cin >> N && N != 0) {
+        for (int i = 0; i < N; i++)
+            cout << 1;
+        cout << endl;
+    }
+
+    return 0;
+}
