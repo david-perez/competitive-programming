fa1b1cd4d356fc22abd00798c412f0ffeb1b7969
david-perez <hallaplay835@gmail.com>
2017-04-12 13:56:31 +0200

    13187 - Felipe and the Sequence https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=871&page=show_problem&problem=5098

---
+++ UVa/013187.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        ll S;
+        cin >> S;
+        cout << S*(S+2) << endl;
+    }
+
+    return 0;
+}
