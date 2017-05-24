2ad895f845eaf95ae2d5c1945ca28951f58706c6
david-perez <hallaplay835@gmail.com>
2017-05-24 15:52:12 +0200

    Refactor UVa 673

--- UVa/000673.cpp
+++ UVa/000673.cpp
@@ -1,36 +1,7 @@
 #include <iostream>
-#include <iomanip>
-#include <sstream>
-#include <cstdlib>
-#include <cstdio>
-#include <vector>
-#include <queue>
-#include <map>
-#include <set>
 #include <stack>
-#include <algorithm> // max...
-#include <utility> // pair
-#include <complex>
-#include <climits> // int, ll...
-#include <limits> // double...
-#include <cmath> // abs, atan...
-#include <cstring> // memset
-#include <string>
-#include <functional> // greater, less...
-#include <bitset>
 
 using namespace std;
-
-typedef long long ll;
-typedef pair<int, int> ii;
-typedef pair<ll, ll> ll_ll;
-typedef pair<double, double> dd;
-typedef vector<int> vi;
-typedef map<int, int> mii;
-typedef vector<ii> vii;
-typedef vector<dd> vdd;
-typedef vector<ll> vll;
-typedef vector<vi> vvi;
 
 int main() {
     int n;
@@ -42,9 +13,9 @@ int main() {
         bool ok = true;
         
         getline(cin, s);
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '(' || s[i] == '[') st.push(s[i]);
-            else if (st.empty() || (st.top() == '(' && s[i] == ']') || (st.top() == '[' && s[i] == ')')) {
+        for (char &c : s) {
+            if (c == '(' || c == '[') st.push(c);
+            else if (st.empty() || (st.top() == '(' && c == ']') || (st.top() == '[' && c == ')')) {
                 ok = false;
                 break;
             } else st.pop();
