697d1561238b1da026b61cee60719317225381d5
david-perez <hallaplay835@gmail.com>
2017-06-07 00:21:07 +0200

    Refactor UVa 10107

--- UVa/010107.cpp
+++ UVa/010107.cpp
@@ -1,45 +1,19 @@
 #include <iostream>
-#include <iomanip>
-#include <sstream>
-#include <cstdlib>
-#include <cstdio>
 #include <vector>
-#include <queue>
-#include <map>
-#include <set>
-#include <stack>
-#include <algorithm> // max...
-#include <utility> // pair
-#include <complex>
-#include <climits> // int, ll...
-#include <limits> // double...
-#include <cmath> // abs, atan...
-#include <cstring> // memset
-#include <string>
-#include <functional> // greater, less...
-#include <bitset>
+#include <algorithm>
 
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> ii;
-typedef pair<ll, ll> ll_ll;
-typedef pair<double, double> dd;
 typedef vector<int> vi;
-typedef map<int, int> mii;
-typedef vector<ii> vii;
-typedef vector<dd> vdd;
-typedef vector<ll> vll;
-typedef vector<vi> vvi;
 
 int main() {
-    long n;
-    vector<long> v;
-    while (cin >> n) {
-        v.push_back(n);
+    vi v;
+    int x;
+    while (cin >> x) {
+        v.push_back(x);
         sort(v.begin(), v.end());
-        cout << (v.size() % 2 == 0 ? (v[v.size()/2]+v[v.size()/2-1])/2 : v[v.size()/2]) << endl;
+        cout << (v.size() % 2 == 0 ? (v[v.size()/2 - 1] + v[v.size()/2]) / 2 : v[v.size()/2]) << endl;
     }
-    
+
     return 0;
 }
